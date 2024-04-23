75c96eb548b2002a5b1f3625273d6aca38b67bf5
Curtis Edwards <curtis-edwards>
2024-04-23 10:00:06 -0400

    Added code

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+/*
+* This program.
+*
+* @author  Curtis Edwards
+* @version 1.0
+* @since   2024-04-23
+*/
+
+function reverseString(stringToReverse: string): string {
+    /*
+    * This function reverses a string, using recursion
+    * @param accepts a string
+    * @return the string backwards, using recursion
+    */
+    if (stringToReverse === "") {
+        return ""
+    } else {
+        let firstCharacterOfString: string = stringToReverse[0]
+        let restOfString: string = stringToReverse.slice(1)
+        
+        return reverseString(restOfString) + firstCharacterOfString
+    }
+}
+
+let aString = "recusrsion"
+
+console.log(`\nThe original string is: ${aString}`)
+let theReverseString = reverseString(aString)
+console.log(`The reversed string is: ${theReverseString}`)
+
+console.log(`\nDone.`)
